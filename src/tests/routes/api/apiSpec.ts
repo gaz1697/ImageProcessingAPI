9c7b04909fd1d41b583a3e95a2370ddd4f1137d6
gaz1697 <gazlan111@gmail.com>
2024-10-18 23:58:20 +0300

    .

--- src/tests/routes/api/apiSpec.ts
+++ src/tests/routes/api/apiSpec.ts
@@ -4,7 +4,7 @@ import app from '../../../app';
 const request = supertest(app);
 
 describe('testing api images processing functionality', () => {
-  it('gets an jpeg/image in response to a correct request', async () => {
+  it('gets a jpeg/image in response to a correct request', async () => {
     const response = await request.get('/api/images?filename=fjord&&width=300&&height=700');
     expect(response.header['content-type']).toEqual('image/jpeg');
   });
